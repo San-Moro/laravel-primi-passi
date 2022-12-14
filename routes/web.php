be68c55018ea3e3b709ffc6ec94c9675b7d56828
San-Moro <valentinacortese.dsg@gmail.com>
2022-12-14 17:46:00 +0100

    bonus

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,25 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    $pageTitle = "Home";
+    $description = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque, illum optio cumque saepe officia quasi laboriosam nostrum pariatur deserunt ullam!";
+    return view('home', compact("pageTitle", "description"));
+})->name("home");
 
-Route::get('/home', function () {
-    return view('home');
-});
+Route::get('/about', function () {
+    $pageTitle = "About";
+    $description = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque, illum optio cumque saepe officia quasi laboriosam nostrum pariatur deserunt ullam!";
+    return view('about', compact("pageTitle", "description"));
+})->name("about");
+
+Route::get('/services', function () {
+    $pageTitle = "Services";
+    $description = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque, illum optio cumque saepe officia quasi laboriosam nostrum pariatur deserunt ullam!";
+    return view('services', compact("pageTitle", "description"));
+})->name("services");
+
+Route::get('/contacts', function () {
+    $pageTitle = "Contacts";
+    $description = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque, illum optio cumque saepe officia quasi laboriosam nostrum pariatur deserunt ullam!";
+    return view('contacts', compact("pageTitle", "description"));
+})->name("contacts");
